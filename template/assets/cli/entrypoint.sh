9c63b39d44c342ff8093d449981d1f2cba017f9e
Alexander Stehlik <a.stehlik@basecom.de>
2022-09-15 15:13:02 +0200

    Improve stability of CLI user adjustments
    Use exising user / group if available.

--- template/assets/cli/entrypoint.sh
+++ template/assets/cli/entrypoint.sh
@@ -5,16 +5,32 @@ set -e
 PHP_XDEBUG_ENABLE="${PHP_XDEBUG_ENABLE:-0}"
 PHP_DOWNGRADE_COMPOSER="${PHP_DOWNGRADE_COMPOSER:-0}"
 
+debug() {
+    if [ -n "${DEBUG_ENTRYPOINT}" ] && [ "${DEBUG_ENTRYPOINT}" -eq 1 ]; then
+        echo "$1"
+    fi
+}
+
 if [ "$PHP_XDEBUG_ENABLE" -eq "1" ]; then
         phpenmod xdebug
 fi
 
 if [ -n "${PHP_LOCALUSER_UID+x}" ]; then
-        sudo usermod -u "${PHP_LOCALUSER_UID}" localuser
+        debug "Adjusting uid of localuser if user with uid ${PHP_LOCALUSER_UID} does not exist..."
+        id "${PHP_LOCALUSER_UID}" > /dev/null 2>&1 || sudo usermod -u "${PHP_LOCALUSER_UID}" localuser
 fi
 
 if [ -n "${PHP_LOCALUSER_GID+x}" ]; then
-        sudo groupmod -g "${PHP_LOCALUSER_GID}" localuser
+        fpmGroupData="$(getent group "${PHP_LOCALUSER_GID}" || echo '')"
+        fpmGroup="${fpmGroupData%%:*}"
+        if [ "$fpmGroup" = ""  ]; then
+                debug "Adjusting gid of localuser group to ${PHP_LOCALUSER_GID}..."
+                fpmGroup="localuser"
+                sudo groupmod -g "${PHP_LOCALUSER_GID}" localuser
+        fi
+        fpmUser="$(id -nu "${PHP_LOCALUSER_UID}")"
+        debug "Adding user $fpmUser to group $fpmGroup"
+        usermod -g "$fpmGroup" "$fpmUser"
 fi
 
 if [ "$PHP_DOWNGRADE_COMPOSER" -eq "1" ]; then

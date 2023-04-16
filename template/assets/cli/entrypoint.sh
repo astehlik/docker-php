c8fbdf56ed9d3aa5bd45704137346fa5eb2556eb
Alexander Stehlik <alex@stehlik-online.de>
2023-04-16 20:46:51 +0200

    Reformat entrypoint scripts

--- template/assets/cli/entrypoint.sh
+++ template/assets/cli/entrypoint.sh
@@ -6,44 +6,44 @@ PHP_XDEBUG_ENABLE="${PHP_XDEBUG_ENABLE:-0}"
 PHP_DOWNGRADE_COMPOSER="${PHP_DOWNGRADE_COMPOSER:-0}"
 
 debug() {
-    if [ -n "${DEBUG_ENTRYPOINT}" ] && [ "${DEBUG_ENTRYPOINT}" -eq 1 ]; then
-        echo "$1"
-    fi
+  if [ -n "${DEBUG_ENTRYPOINT}" ] && [ "${DEBUG_ENTRYPOINT}" -eq 1 ]; then
+    echo "$1"
+  fi
 }
 
 if [ "$PHP_XDEBUG_ENABLE" -eq "1" ]; then
-        phpenmod xdebug
+  phpenmod xdebug
 fi
 
 if [ -n "${PHP_LOCALUSER_UID+x}" ]; then
-        debug "Adjusting uid of localuser if user with uid ${PHP_LOCALUSER_UID} does not exist..."
-        id "${PHP_LOCALUSER_UID}" > /dev/null 2>&1 || usermod -u "${PHP_LOCALUSER_UID}" localuser
+  debug "Adjusting uid of localuser if user with uid ${PHP_LOCALUSER_UID} does not exist..."
+  id "${PHP_LOCALUSER_UID}" >/dev/null 2>&1 || usermod -u "${PHP_LOCALUSER_UID}" localuser
 fi
 
 if [ -n "${PHP_LOCALUSER_GID+x}" ]; then
-        cliGroupData="$(getent group "${PHP_LOCALUSER_GID}" || echo '')"
-        cliGroup="${cliGroupData%%:*}"
-        if [ "$cliGroup" = ""  ]; then
-                debug "Adjusting gid of localuser group to ${PHP_LOCALUSER_GID}..."
-                cliGroup="localuser"
-                groupmod -g "${PHP_LOCALUSER_GID}" localuser
-        fi
-        cliUser="$(id -nu "${PHP_LOCALUSER_UID}")"
-        debug "Adding user $cliUser to group $cliGroup"
-        usermod -g "$cliGroup" "$cliUser" > /dev/null
+  cliGroupData="$(getent group "${PHP_LOCALUSER_GID}" || echo '')"
+  cliGroup="${cliGroupData%%:*}"
+  if [ "$cliGroup" = "" ]; then
+    debug "Adjusting gid of localuser group to ${PHP_LOCALUSER_GID}..."
+    cliGroup="localuser"
+    groupmod -g "${PHP_LOCALUSER_GID}" localuser
+  fi
+  cliUser="$(id -nu "${PHP_LOCALUSER_UID}")"
+  debug "Adding user $cliUser to group $cliGroup"
+  usermod -g "$cliGroup" "$cliUser" >/dev/null
 fi
 
 if [ "$PHP_DOWNGRADE_COMPOSER" -eq "1" ]; then
-        composer self-update --1
+  composer self-update --1
 fi
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-        set -- php "$@"
+  set -- php "$@"
 fi
 
 if [ -n "${PHP_LOCALUSER_UID+x}" ]; then
-        exec sudo --preserve-env -H -u localuser "$@"
+  exec sudo --preserve-env -H -u localuser "$@"
 else
-        exec "$@"
+  exec "$@"
 fi
